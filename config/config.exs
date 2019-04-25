c1a19c65d4db156aaceadc1a4a7d3032c8876e44
LoranceChen <w68055010@qq.com>
2019-04-25 23:24:32 +0800

    init phoenix framework

---
+++ config/config.exs
@@ -0,0 +1,30 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :phx_blog,
+  ecto_repos: [PhxBlog.Repo]
+
+# Configures the endpoint
+config :phx_blog, PhxBlogWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "v1hTt5g22z3QISEgdi4JSObvvcSjiKc4fYBZ161/GAmINhvaRpd+GW+x4LQitTsp",
+  render_errors: [view: PhxBlogWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: PhxBlog.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
